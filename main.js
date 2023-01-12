62d00babe999e9a0086ba1e761bf3cfd917923d0
lineiika <112777376+lineiika@users.noreply.github.com>
2023-01-12 17:15:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let userAnswer =  Number(prompt('Ваше число?'));
+let userAnswer2 = Number(prompt('Ваше число?'));
+
+
+alert(`${userAnswer} + ${userAnswer} = ${userAnswer + userAnswer2}
+${userAnswer} - ${userAnswer}= ${userAnswer - userAnswer2}
+${userAnswer} * ${userAnswer} = ${userAnswer * userAnswer2}
+${userAnswer} / ${userAnswer} = ${userAnswer / userAnswer2}`); 
